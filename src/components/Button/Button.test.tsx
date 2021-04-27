a9e5dd6ce6460b8061da71391f96f10a30f5bf03
Ashish D'sa <ashd621@gmail.com>
2021-04-28 02:29:48 +0530

    Updated src/components/Button/Button.test.tsx

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,15 @@
+//import * as React from "react";
+
+//import Button from "./Button";
+
+//describe("Button", () => {
+//  it("should render correctly", () => {
+//   const component = (
+//      <Button>
+//        Test
+//      </Button>,
+//    ).toJSON();
+
+//    expect(component).toMatchSnapshot();
+//  });
+//});
