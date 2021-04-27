a50a2295b3b9f7710035369bd55cbbda6af6b139
Ashish D'sa <ashd621@gmail.com>
2021-04-28 02:29:48 +0530

    Updated src/components/Button/Button.stories.tsx

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,16 @@
+import {boolean, color, select, text, withKnobs} from "@storybook/addon-knobs";
+import {storiesOf} from "@storybook/react-native";
+import * as React from "react";
+import {Button as PaperButton} from "react-native-paper";
+
+const stories = storiesOf("Components/Button", module);
+stories.addDecorator(withKnobs);
+
+stories.add("Button", () => (
+  <PaperButton
+    color={color("Color", "blue")}
+    disabled={boolean("Disabled", false)}
+    mode={select("Mode", ["text", "outlined", "contained"], "text")}>
+    {text("Label", "Default")}
+  </PaperButton>
+));
