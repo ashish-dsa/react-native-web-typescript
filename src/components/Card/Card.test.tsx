eb07f4f189e4d2055ebf61d8e639dca5f4acb6f4
Ashish D'sa <ashd621@gmail.com>
2021-05-07 04:27:09 +0530

    Updated src/components/Card/Card.test.tsx

--- src/components/Card/Card.test.tsx
+++
@@ -1,9 +0,0 @@
-import Card from "./Card";
-
-describe("Card", () => {
-  it("should render correctly", () => {
-    const component = <Card>Test</Card>;
-
-    expect(component).toMatchSnapshot();
-  });
-});
