d1511816ab58f317885f19df2429f6acca617169
Ashish D'sa <ashd621@gmail.com>
2021-05-07 04:27:09 +0530

    Updated craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -13,32 +13,6 @@ module.exports = {
       remove: [] /* An array of plugin constructor's names (i.e. "StyleLintPlugin", "ESLintWebpackPlugin" ) */,
     },
     configure: (webpackConfig, {env, paths}) => {
-      // your overridden `style-loader`
-      const overrideOptions = {
-        loader: "babel-loader",
-        options: {
-          // Disable reading babel configuration
-          babelrc: false,
-          configFile: false,
-
-          // The configuration for compilation
-          presets: [
-            ["@babel/preset-env", {useBuiltIns: "usage"}],
-            "@babel/preset-react",
-            "@babel/preset-flow",
-            "@babel/preset-typescript",
-          ],
-          plugins: ["@babel/plugin-proposal-class-properties", "@babel/plugin-proposal-object-rest-spread"],
-        },
-      };
-
-      // override `style-loader`
-      const {isFound, match} = getLoader(webpackConfig, loaderByName("babel-loader"));
-
-      if (isFound) {
-        match.parent[match.index] = overrideOptions;
-      }
-
       return webpackConfig;
     },
   },
