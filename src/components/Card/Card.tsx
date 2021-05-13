0c68f73dcbc5ac9e892185cef5df0d5cf7dc64b4
Ashish D'sa <ashd621@gmail.com>
2021-05-13 21:32:42 +0530

    Updated src/components/Card/Card.tsx

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement, useEffect } from "react";
+
+interface ICardProps {
+
+}
+const Card: React.FC<ICardProps> = (props):ReactElement => {
+
+  
+  useEffect(() => {
+
+  return () => {};
+  }, []);
+
+  return (
+       <>
+       </>
+    )
+
+};
+
+  export default Card;
