441be48e8cde32f7d3e28bf35e464ba7d8d332a6
Ashish D'sa <ashd621@gmail.com>
2021-05-13 23:06:15 +0530

    Updated src/components/Card/Card.test.tsx

---
+++ src/components/Card/Card.test.tsx
@@ -0,0 +1,14 @@
+
+import Card from "./Card";
+
+describe("Card", () => {
+it("should render correctly", () => {
+   const component = (
+      <Card>
+        Test
+      </Card>
+    );
+
+    expect(component).toMatchSnapshot();
+  });
+});
