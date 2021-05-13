7023e2e39b9f2c8c4c7f74f7e608d8fae880ceb1
Ashish D'sa <ashd621@gmail.com>
2021-05-13 21:32:43 +0530

    Updated src/components/ProgressBar/ProgressBar.tsx

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement, useEffect } from "react";
+
+interface IProgressBarProps {
+
+}
+const ProgressBar: React.FC<IProgressBarProps> = (props):ReactElement => {
+
+  
+  useEffect(() => {
+
+  return () => {};
+  }, []);
+
+  return (
+       <>
+       </>
+    )
+
+};
+
+  export default ProgressBar;
