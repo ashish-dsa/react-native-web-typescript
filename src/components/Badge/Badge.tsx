3f6a2de69468d96b675ae3301273acd25edd1e04
Ashish D'sa <ashd621@gmail.com>
2021-05-13 21:32:42 +0530

    Updated src/components/Badge/Badge.tsx

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement, useEffect } from "react";
+
+interface IBadgeProps {
+
+}
+const Badge: React.FC<IBadgeProps> = (props):ReactElement => {
+
+  
+  useEffect(() => {
+
+  return () => {};
+  }, []);
+
+  return (
+       <>
+       </>
+    )
+
+};
+
+  export default Badge;
