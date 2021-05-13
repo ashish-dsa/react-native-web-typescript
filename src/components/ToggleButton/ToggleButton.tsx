3e207fd9f900397ddc349f02cce2e19db98a5736
Ashish D'sa <ashd621@gmail.com>
2021-05-13 21:32:43 +0530

    Updated src/components/ToggleButton/ToggleButton.tsx

---
+++ src/components/ToggleButton/ToggleButton.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement, useEffect } from "react";
+
+interface IToggleButtonProps {
+
+}
+const ToggleButton: React.FC<IToggleButtonProps> = (props):ReactElement => {
+
+  
+  useEffect(() => {
+
+  return () => {};
+  }, []);
+
+  return (
+       <>
+       </>
+    )
+
+};
+
+  export default ToggleButton;
