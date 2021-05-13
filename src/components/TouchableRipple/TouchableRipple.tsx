b7f474d18e63ba9d8327c6bf5a1f4ffbcb067818
Ashish D'sa <ashd621@gmail.com>
2021-05-13 21:32:43 +0530

    Updated src/components/TouchableRipple/TouchableRipple.tsx

---
+++ src/components/TouchableRipple/TouchableRipple.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement, useEffect } from "react";
+
+interface ITouchableRippleProps {
+
+}
+const TouchableRipple: React.FC<ITouchableRippleProps> = (props):ReactElement => {
+
+  
+  useEffect(() => {
+
+  return () => {};
+  }, []);
+
+  return (
+       <>
+       </>
+    )
+
+};
+
+  export default TouchableRipple;
