41389d1aeda4b162e322fca5f451e45e82b0b455
Ashish D'sa <ashd621@gmail.com>
2021-05-13 21:32:43 +0530

    Updated src/components/Switch/Switch.tsx

---
+++ src/components/Switch/Switch.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement, useEffect } from "react";
+
+interface ISwitchProps {
+
+}
+const Switch: React.FC<ISwitchProps> = (props):ReactElement => {
+
+  
+  useEffect(() => {
+
+  return () => {};
+  }, []);
+
+  return (
+       <>
+       </>
+    )
+
+};
+
+  export default Switch;
