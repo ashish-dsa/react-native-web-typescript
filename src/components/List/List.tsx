f9f9f9574765b21114110d5ea4f7edf961cc3c5e
Ashish D'sa <ashd621@gmail.com>
2021-05-13 21:32:43 +0530

    Updated src/components/List/List.tsx

---
+++ src/components/List/List.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement, useEffect } from "react";
+
+interface IListProps {
+
+}
+const List: React.FC<IListProps> = (props):ReactElement => {
+
+  
+  useEffect(() => {
+
+  return () => {};
+  }, []);
+
+  return (
+       <>
+       </>
+    )
+
+};
+
+  export default List;
