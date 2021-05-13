21f54e7985d1e200d2f537f0e2f418e343cb5626
Ashish D'sa <ashd621@gmail.com>
2021-05-13 21:28:09 +0530

    Updated src/components/Button/Button.tsx

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement, useEffect } from "react";
+
+interface IButtonProps {
+
+}
+const Button: React.FC<IButtonProps> = (props):ReactElement => {
+
+  
+  useEffect(() => {
+
+  return () => {};
+  }, []);
+
+  return (
+       <>
+       </>
+    )
+
+};
+
+  export default Button;
