964c1cc5cebf205d9c2f9763cf12b45b6d7a66d8
Ashish D'sa <ashd621@gmail.com>
2021-05-13 21:32:42 +0530

    Updated src/components/Dialog/Dialog.tsx

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement, useEffect } from "react";
+
+interface IDialogProps {
+
+}
+const Dialog: React.FC<IDialogProps> = (props):ReactElement => {
+
+  
+  useEffect(() => {
+
+  return () => {};
+  }, []);
+
+  return (
+       <>
+       </>
+    )
+
+};
+
+  export default Dialog;
