458c91b7ba541939200eb45d7c09370a430be2f5
Ashish D'sa <ashd621@gmail.com>
2021-05-13 21:32:43 +0530

    Updated src/components/Snackbar/Snackbar.tsx

---
+++ src/components/Snackbar/Snackbar.tsx
@@ -0,0 +1,21 @@
+import React, { ReactElement, useEffect } from "react";
+
+interface ISnackbarProps {
+
+}
+const Snackbar: React.FC<ISnackbarProps> = (props):ReactElement => {
+
+  
+  useEffect(() => {
+
+  return () => {};
+  }, []);
+
+  return (
+       <>
+       </>
+    )
+
+};
+
+  export default Snackbar;
