9b0856b05d50aecfcbda4e953fdd490d8ca6dc2e
Ashish D'sa <ashd621@gmail.com>
2021-05-15 01:30:58 +0530

    Updated src/navigation/BaseNavigator.tsx

--- src/navigation/BaseNavigator.tsx
+++ src/navigation/BaseNavigator.tsx
@@ -2,18 +2,20 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator, TransitionPresets} from "@react-navigation/stack";
 import React from "react";
 import "react-native";
-import Home from "screens/Home";
+import ExampleScreen1 from "screens/ExampleScreen1";
+import ExampleScreen2 from "screens/ExampleScreen2";
 import {ROUTES} from "./constants";
 
-export const BaseNavigator = () => {
+export const BaseNavigator = ({theme}: {theme: Theme}) => {
   const Stack = createStackNavigator();
   const TransitionScreenOptions = {
     ...TransitionPresets.SlideFromRightIOS, // This is where the transition happens
   };
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={theme}>
       <Stack.Navigator screenOptions={{...TransitionScreenOptions, headerShown: false}}>
-        <Stack.Screen name={ROUTES.Home} component={Home} />
+        <Stack.Screen name={ROUTES.ExampleScreen1} component={ExampleScreen1} />
+        <Stack.Screen name={ROUTES.ExampleScreen2} component={ExampleScreen2} />
       </Stack.Navigator>
     </NavigationContainer>
   );
