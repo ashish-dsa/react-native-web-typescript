ea0f2d9489bad9834f0a88a3706631b4db1acc46
Ashish D'sa <ashd621@gmail.com>
2021-05-14 17:49:15 +0530

    Updated src/components/Button/Button.stories.tsx

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,15 +1,30 @@
 import {boolean, color, select, text, withKnobs} from "@storybook/addon-knobs";
 import {storiesOf} from "@storybook/react-native";
+import React, {ReactElement} from "react";
 import {Button as PaperButton} from "react-native-paper";
+import iconFont from "react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf";
 
 const stories = storiesOf("Components/Button", module);
 stories.addDecorator(withKnobs);
 
-stories.add("Button", () => (
-  <PaperButton
-    color={color("Color", "blue")}
-    disabled={boolean("Disabled", false)}
-    mode={select("Mode", ["text", "outlined", "contained"], "contained")}>
-    {text("Label", "Default")}
-  </PaperButton>
-));
+interface ICardProps {}
+const buttonStory: React.FC<any> = (): ReactElement => {
+  return (
+    <>
+      <style type="text/css">{`
+         @font-face {
+          src: url(${iconFont});
+          font-family: MaterialCommunityIcons;
+        }
+       `}</style>
+      <PaperButton
+        color={color("Color", "blue")}
+        disabled={boolean("Disabled", false)}
+        mode={select("Mode", ["text", "outlined", "contained"], "contained")}
+        icon="camera">
+        {text("Label", "Default")}
+      </PaperButton>
+    </>
+  );
+};
+stories.add("Button", buttonStory);
