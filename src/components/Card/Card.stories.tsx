4d111def5ba98321ca686d062790963c6f8e2716
Ashish D'sa <ashd621@gmail.com>
2021-05-14 17:49:15 +0530

    Updated src/components/Card/Card.stories.tsx

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,13 +1,22 @@
-import {boolean, color, select, text, withKnobs} from "@storybook/addon-knobs";
-
+import {withKnobs} from "@storybook/addon-knobs";
 import {storiesOf} from "@storybook/react-native";
-import Card from "./Card";
+// import Card from "./Card";
+import {Button, Card, Paragraph, Title} from "react-native-paper";
 
 const stories = storiesOf("Components/Card", module);
 stories.addDecorator(withKnobs);
 
-stories.add("Card",()=>(
-    <Card>
-    
-    </Card>
+stories.add("Card", () => (
+  <Card>
+    <Card.Title title="Card Title" subtitle="Card Subtitle" />
+    <Card.Content>
+      <Title>Card title</Title>
+      <Paragraph>Card content</Paragraph>
+    </Card.Content>
+    <Card.Cover source={{uri: "https://picsum.photos/700"}} />
+    <Card.Actions>
+      <Button>Cancel</Button>
+      <Button>Ok</Button>
+    </Card.Actions>
+  </Card>
 ));
