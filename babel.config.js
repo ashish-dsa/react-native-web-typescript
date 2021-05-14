07b167fabb4a9065221318943d48a1f9b292d12c
Ashish D'sa <ashd621@gmail.com>
2021-05-15 01:30:58 +0530

    Updated babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -21,8 +21,10 @@ module.exports = {
     },
   },
   plugins: [
+    ["@babel/plugin-transform-flow-strip-types", {loose: true}],
     ["@babel/plugin-proposal-class-properties", {loose: true}],
     ["@babel/plugin-proposal-private-methods", {loose: true}],
     ["@babel/plugin-proposal-private-property-in-object", {loose: true}],
+    ["@babel/plugin-proposal-decorators", {legacy: true}],
   ],
 };
