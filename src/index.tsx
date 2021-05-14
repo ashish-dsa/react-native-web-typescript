f86fefe78a1474012824efafaaed0afa127a2f30
Ashish D'sa <ashd621@gmail.com>
2021-05-14 17:49:15 +0530

    Updated src/index.tsx

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import {AppRegistry, Platform} from "react-native";
-import App from "./App";
-
-AppRegistry.registerComponent("myApp", () => App);
-if (Platform.OS === "web") {
-  AppRegistry.runApplication("myApp", {
-    rootTag: document.getElementById("root"),
-  });
-}
