7ad608fe758f7a16023582839bfce22a16923c65
Ashish D'sa <ashd621@gmail.com>
2021-05-16 16:47:52 +0530

    Updated src/components/Card/CardCover.tsx

--- src/components/Card/CardCover.tsx
+++ src/components/Card/CardCover.tsx
@@ -3,7 +3,6 @@ import {Card as PaperCard} from "react-native-paper";
 
 type CardCover = React.ComponentProps<typeof PaperCard.Cover>;
 
-const CardCover: React.FC<CardCover> = (props): ReactElement => {
+export const CardCover: React.FC<CardCover> = (props): ReactElement => {
   return <PaperCard.Cover {...props} />;
 };
-export default CardCover;
