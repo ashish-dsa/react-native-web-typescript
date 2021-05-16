9189472702be01c504ea19b79b342816fb4ace64
Ashish D'sa <ashd621@gmail.com>
2021-05-16 16:47:52 +0530

    Updated src/components/Card/index.tsx

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,1 +1,5 @@
 export {default} from "./Card";
+export {CardActions} from "./CardActions";
+export {CardContent} from "./CardContent";
+export {CardCover} from "./CardCover";
+export {CardTitle} from "./CardTitle";
