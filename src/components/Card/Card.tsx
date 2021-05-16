5bcde1120e33213bdf3615ef7795daaad3658071
Ashish D'sa <ashd621@gmail.com>
2021-05-16 16:45:02 +0530

    Updated src/components/Card/Card.tsx

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,14 @@
-import React, { ReactElement, useEffect } from "react";
+import React, {ReactElement} from "react";
+import {Card as PaperCard} from "react-native-paper";
 
-interface ICardProps {
+type DefaultCardProps = React.ComponentProps<typeof PaperCard>;
+// type CardTitle = React.ComponentProps<typeof PaperCard.Title>;
+// type CardContent = React.ComponentProps<typeof PaperCard.Content>;
+// type CardActions = React.ComponentProps<typeof PaperCard.Actions>;
+// type CardCover = React.ComponentProps<typeof PaperCard.Cover>;
+export type CardProps = DefaultCardProps;
 
-}
-const Card: React.FC<ICardProps> = (props):ReactElement => {
-
-  
-  useEffect(() => {
-
-  return () => {};
-  }, []);
-
-  return (
-       <>
-       </>
-    )
-
+const Card: React.FC<CardProps> = (props): ReactElement => {
+  return <PaperCard {...props} />;
 };
-
-  export default Card;
+export default Card;
