a760792a1ae57a28ec3c2ccb6d9f6c29718a97ac
Ashish D'sa <ashd621@gmail.com>
2021-05-16 16:47:52 +0530

    Updated src/components/Card/Card.tsx

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,7 @@
 import React, {ReactElement} from "react";
 import {Card as PaperCard} from "react-native-paper";
 
-type DefaultCardProps = React.ComponentProps<typeof PaperCard>;
-// type CardTitle = React.ComponentProps<typeof PaperCard.Title>;
-// type CardContent = React.ComponentProps<typeof PaperCard.Content>;
-// type CardActions = React.ComponentProps<typeof PaperCard.Actions>;
-// type CardCover = React.ComponentProps<typeof PaperCard.Cover>;
-export type CardProps = DefaultCardProps;
+export type CardProps = React.ComponentProps<typeof PaperCard>;
 
 const Card: React.FC<CardProps> = (props): ReactElement => {
   return <PaperCard {...props} />;
