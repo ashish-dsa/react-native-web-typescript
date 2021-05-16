006c521d8a001b32617a57b96be7080ab646301e
Ashish D'sa <ashd621@gmail.com>
2021-05-16 16:45:02 +0530

    Updated src/screens/ExampleScreen1/ExampleScreen1.tsx

--- src/screens/ExampleScreen1/ExampleScreen1.tsx
+++ src/screens/ExampleScreen1/ExampleScreen1.tsx
@@ -1,8 +1,13 @@
 import {NavigationProp} from "@react-navigation/core";
+import Button from "components/Button";
+import Card from "components/Card";
+import CardActions from "components/Card/CardActions";
+import CardCover from "components/Card/CardCover";
+import CardTitle from "components/Card/CardTitle";
 import {ROUTES} from "navigation/constants";
 import {default as React, ReactElement, useEffect} from "react";
 import {StyleSheet, View} from "react-native";
-import {Appbar, Button, Card} from "react-native-paper";
+import {Appbar} from "react-native-paper";
 
 interface IExampleScreen1Props {
   navigation: NavigationProp<any>;
@@ -23,16 +28,16 @@ const ExampleScreen1: React.FC<IExampleScreen1Props> = (props): ReactElement => 
       {[1, 1, 1].map((value, index) => {
         return (
           <Card style={styles.card} key={index}>
-            <Card.Title title="Card Title" subtitle="Card Subtitle" />
-            <Card.Cover source={{uri: "https://picsum.photos/70" + index.toString()}} />
-            <Card.Actions>
+            <CardTitle title="Card Title" subtitle="Card Subtitle" />
+            <CardCover source={{uri: "https://picsum.photos/70" + index.toString()}} />
+            <CardActions>
               <Button
                 icon="page-next-outline"
                 mode="contained"
                 onPress={() => props.navigation.navigate(ROUTES.ExampleScreen2)}>
                 Go to next screen
               </Button>
-            </Card.Actions>
+            </CardActions>
           </Card>
         );
       })}
